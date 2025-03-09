b7d844ffb98b34d8d0e3e76c6537bcc5717fdbe7
Maksim Hayytbayev <maksim.hayytbayev@outlook.com>
2025-03-09 18:24:24 +0100

    nodejs

---
+++ index.js
@@ -0,0 +1,71 @@
+/*
+const my_math = require('./my_math');
+
+let res1 = my_math.plus(5,5);
+console.log(res1);
+
+let res2 = my_math.minus(5,5);
+console.log(res2);
+*/
+      //const fs = require('fs');
+/*
+let res = fs.readFileSync('some.txt','utf-8');
+console.log(res);
+
+fs.writeFileSync('some.txt','hi my name is ');
+*/
+
+/*
+fs.readFile('some.txt','utf-8',(err,data)=>{
+    fs.writeFile('some.txt',data + '\nHello',(err,data)=>{
+        console.log("OK");
+    });
+});
+*/
+/*
+fs.mkdir('text-file', ()=>{
+    fs.writeFile('./text-file/some.txt', '1234567890', ()=>{});
+});
+*/
+/*
+fs.unlink('./text-file/some.txt', ()=>{
+    fs.rmdir('./text-file',()=>{})
+});
+*/
+
+
+const http = require('http')
+const fs = require('fs')
+
+let server = http.createServer((req, res)=>{
+   /*
+    res.writeHead(200,{'Content-Type':'text/plain; charset=utf-8'})
+    res.end('Hello Node.js')
+    */
+     //res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'})
+    //res.end('Hello <b>Node.js</b>')
+    /*
+    res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'})
+    const stream = fs.createReadStream('./templates/index.html')
+    stream.pipe(res)
+    */
+   res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'})
+
+   if(req.url =='/')
+       fs.createReadStream('./templates/index.html').pipe(res)
+   else if(req.url =='/about')
+       fs.createReadStream('./templates/about.html').pipe(res)
+   else
+    fs.createReadStream('./templates/erorr.html').pipe(res)
+})
+
+const PORT = 3001;
+const HOST = 'localhost'; //127.0.0.1
+
+server.listen(PORT, HOST, ()=>{
+    console.log(`Server started:  http:${PORT}:${HOST}`);
+})
+
+
+//console.log(currencyConverter);
+
---
+++ templates/my_math.js
@@ -0,0 +1,13 @@
+const plus =(a,b)=>{
+    return a+b;
+}
+
+const minus =(a,b)=>{
+    return a-b;
+}
+
+module.exports = {
+ plus: plus,
+ minus: minus
+    
+}
